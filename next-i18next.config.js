c284e998d1e236b59ab66ef0a92a0dc60591889d
Suat-17 <yargicisuat@gmail.com>
2023-02-27 11:17:24 +0300

    first commit

---
+++ next-i18next.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en","tr"],
+    localePath: path.resolve("./public/locales"),
+    reloadOnPrerender: true,
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import { appWithTranslation } from 'next-i18next'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+
+
+const MyApp = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+)
+
+export default appWithTranslation(MyApp)
